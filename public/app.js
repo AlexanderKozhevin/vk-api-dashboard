13b4d676fba371471321da65f7ec77840d5fdc2e
alexanderkozhevin <turboasm21@gmail.com>
2016-05-04 15:36:23 +0300

    test3

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
-console.log('hello');
+var app = angular.module('app', ['ngMaterial'])
+
+angular.element(document).ready(function(){
+	angular.bootstrap(document, ["app"]);
+})
+
+app.controller('appCtrl', function ($scope, $http) {
+
+  $scope.getData = function(){
+    $http.post("https://evening-citadel-38451.herokuapp.com/getuser", {id: 100500}).then(function(data){
+      console.log(data);
+    })
+  }
+
+})
